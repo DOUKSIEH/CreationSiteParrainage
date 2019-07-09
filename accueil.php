946735ef13afafa2c79f3c093730b85de10ffaf4
DOUKSIEH <douksieha@gmail.com>
2019-07-09 10:55:42 +0200

    correction sur les fichiers accueil et account

--- accueil.php
+++ accueil.php
@@ -1,1 +1,62 @@
 <?php
+
+
+require_once 'template/bootstrap.php';
+
+$session = Session::getInstance();
+
+$auth = App::getAuth();
+
+
+
+//var_dump($auth->user());
+
+
+
+
+ require  "template/header.php" ;?>
+
+<h1>Si longus, levis dictata sunt.</h1>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. At ille non pertimuit saneque fidenter: Istis quidem ipsis verbis, inquit; Paulum, cum regem Persem captum adduceret, eodem flumine invectio? In qua quid est boni praeter summam voluptatem, et eam sempiternam? <a href="http://loripsum.net/" target="_blank">Frater et T.</a> <i>Hoc tu nunc in illo probas.</i> Quae hic rei publicae vulnera inponebat, eadem ille sanabat. Quos nisi redarguimus, omnis virtus, omne decus, omnis vera laus deserenda est. <b>Duo Reges: constructio interrete.</b> Recte dicis; </p>
+
+<p>Cur post Tarentum ad Archytam? <mark>Eam stabilem appellas.</mark> Satis est tibi in te, satis in legibus, satis in mediocribus amicitiis praesidii. <mark>Non est igitur summum malum dolor.</mark> Non igitur bene. Tertium autem omnibus aut maximis rebus iis, quae secundum naturam sint, fruentem vivere. Nam his libris eum malo quam reliquo ornatu villae delectari. <a href="http://loripsum.net/" target="_blank">Verum hoc idem saepe faciamus.</a> Quae sunt igitur communia vobis cum antiquis, iis sic utamur quasi concessis; </p>
+
+<dl>
+    <dt><dfn>Restatis igitur vos;</dfn></dt>
+    <dd>Non enim quaero quid verum, sed quid cuique dicendum sit.</dd>
+    <dt><dfn>Quo tandem modo?</dfn></dt>
+    <dd>Aut, Pylades cum sis, dices te esse Orestem, ut moriare pro amico?</dd>
+    <dt><dfn>Sedulo, inquam, faciam.</dfn></dt>
+    <dd>Idcirco enim non desideraret, quia, quod dolore caret, id in voluptate est.</dd>
+    <dt><dfn>Recte dicis;</dfn></dt>
+    <dd>Ut enim consuetudo loquitur, id solum dicitur honestum, quod est populari fama gloriosum.</dd>
+</dl>
+
+
+<ol>
+    <li>Sin laboramus, quis est, qui alienae modum statuat industriae?</li>
+    <li>Nunc vides, quid faciat.</li>
+    <li>Scientiam pollicentur, quam non erat mirum sapientiae cupido patria esse cariorem.</li>
+</ol>
+
+
+<h2>Illa sunt similia: hebes acies est cuipiam oculorum, corpore alius senescit;</h2>
+
+<p>Sed eum qui audiebant, quoad poterant, defendebant sententiam suam. Dic in quovis conventu te omnia facere, ne doleas. <a href="http://loripsum.net/" target="_blank">Hunc vos beatum;</a> Qui-vere falsone, quaerere mittimus-dicitur oculis se privasse; </p>
+
+<ul>
+    <li>At enim hic etiam dolore.</li>
+    <li>Si enim ita est, vide ne facinus facias, cum mori suadeas.</li>
+</ul>
+
+
+<blockquote cite="http://loripsum.net">
+    Quae est quaerendi ac disserendi, quae logikh dicitur, iste vester plane, ut mihi quidem videtur, inermis ac nudus est.
+</blockquote>
+
+
+
+
+<?php require  "template/footer.php" ;?>
+
